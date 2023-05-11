288d79318ec9cbc649b5918fb2a0103a935636bd
Matt Taylor <mrsymtec@gmail.com>
2023-05-11 22:02:21 +1200

    Completed scriptjs file

--- views/public/public/script.js
+++ views/public/public/script.js
@@ -1,10 +1,36 @@
 window.onload = function () {
-  const converter = new showdown.converter();
+  const converter = new showdown.Converter();
   const pad = document.getElementById("pad");
   const markdownArea = document.getElementById("markdown");
 
-  const convertTextAreaToMaerdown = function () {
-    const markdownText = pad.ariaValueMax;
+  // make the tab act like a tab
+  pad.addEventListener("keydown", function (e) {
+    if (e.keyCode === 9) {
+      // tab was pressed
+      // get caret position/selection
+      const start = this.selectionStart;
+      const end = this.selectionEnd;
+
+      const target = e.target;
+      const value = target.value;
+
+      // set textarea value to: text before caret + tab + text after caret
+      target.value = value.substring(0, start) + "\t" + value.substring(end);
+
+      // put caret at right position again (add one for the tab)
+      this.selectionStart = this.selectionEnd = start + 1;
+
+      // prevent the focus lose
+      e.preventDefault();
+    }
+  });
+
+  const previousMarkdownValue;
+
+  // convert text area to markdown html
+  const convertTextAreaToMarkdown = function () {
+    const markdownText = pad.value;
+    previousMarkdownValue = markdownText;
     html = converter.makeHtml(markdownText);
     markdownArea.innerHTML = html;
   };
@@ -16,22 +42,26 @@ window.onload = function () {
     return false;
   };
 
+  // check every second if the text area has changed
   setInterval(function () {
     if (didChangeOccur()) {
       convertTextAreaToMarkdown();
     }
   }, 1000);
 
-  pad.addEventListener("input", convertTextAreaToMaerdown);
+  // convert textarea on input change
+  pad.addEventListener("input", convertTextAreaToMarkdown);
 
- // ignore if on home page
-    if(document.location.pathname.length > 1){
- // implement share js
-        const documentName = document.location.pathname.substring(1);
-  sharejs.open(document.location.pathname, "text", function (error, doc) {
-    doc.attach_textarea(pad);
-    convertTextAreaToMaerdown();
-  });
+  // ignore if on home page
+  if (document.location.pathname.length > 1) {
+    // implement share js
+    const documentName = document.location.pathname.substring(1);
+    sharejs.open(documentName, "text", function (error, doc) {
+      doc.attach_textarea(pad);
+      convertTextAreaToMarkdown();
+    });
+  }
 
-  
+  // convert on page load
+  convertTextAreaToMarkdown();
 };

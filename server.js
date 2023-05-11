3ad32ebf4ba92dac82df0a91f85ceac808b6efa8
Matt Taylor <mrsymtec@gmail.com>
2023-05-11 19:48:41 +1200

    Added route for ids added sharejs dependencies added code to attach express to server

--- server.js
+++ server.js
@@ -12,6 +12,22 @@ app.get("/", function (require, respond) {
   respond.render("pad");
 });
 
+app.get("/(:id)", function (require, respond) {
+  respond.render("pad");
+});
+
+//Get share dependencies
+const sharejs = require("share");
+require("redis");
+
+//Options for sharejs
+const options = {
+  db: { type: "redis" },
+};
+
+//Attach the express server to sharejs
+sharejs.server.attach(app, options);
+
 //Listen on port 8000 (for local host) or the port defined for heroku
 const port = process.env.PORT || 8000;
 app.listen(port);
